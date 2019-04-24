a22a2b5db1b05ab88784a48cde9212b551e3a64e
CarlosGutierrezArdila <gutierrezcarlos14@gmail.com>
2019-04-24 12:00:15 -0500

    first commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	_ "testApi/routers"
+
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/logs"
+	"github.com/astaxie/beego/orm"
+	"github.com/astaxie/beego/plugins/cors"
+	_ "github.com/lib/pq"
+	"github.com/udistrital/utils_oas/customerror"
+)
+
+func main() {
+	orm.Debug = true
+	orm.RegisterDataBase("default", "postgres", beego.AppConfig.String("sqlconn"))
+	if beego.BConfig.RunMode == "dev" {
+		beego.BConfig.WebConfig.DirectoryIndex = true
+		beego.BConfig.WebConfig.StaticDir["/swagger"] = "swagger"
+	}
+	// se eagrega funcion para permititr consulta desd3e el cliente
+	beego.InsertFilter("*", beego.BeforeRouter, cors.Allow(&cors.Options{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{"PUT", "PATCH", "GET", "POST", "OPTIONS", "DELETE"},
+		AllowHeaders: []string{"Origin", "x-requested-with",
+			"content-type",
+			"accept",
+			"origin",
+			"authorization",
+			"x-csrftoken"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+	}))
+	//logs
+	logs.SetLogger(logs.AdapterFile, `{"filename":"/var/log/beego/testApi/testApi.log"}`)
+	//errores personalizados
+	beego.ErrorController(&customerror.CustomErrorController{})
+	orm.RunCommand()
+	beego.Run()
+}
